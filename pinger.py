77cc2f1b37f994f21f8ed99f4ca919090770996b
keraattin <kerem-oruc@outlook.com>
2020-01-29 22:44:59 +0300

    ping function added

--- pinger.py
+++ pinger.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 
-def main():
-    print("first line")
+import subprocess
 
+def ping(host):
+    count = 3
+
+    # Ping
+    try:
+        cmd_ping = subprocess.check_output(["ping",host,"-c",str(count)])
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+def main():
+    ping("")
 
 if __name__ == "__main__":
     main()
