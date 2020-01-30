a7ced1556360809a13c57461430395789d59b1ab
keraattin <kerem-oruc@outlook.com>
2020-01-30 20:42:23 +0300

    Xlsx files reading completed

--- pinger.py
+++ pinger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import subprocess
+import sys
 
 # Ping command
 def ping(host,count):
@@ -10,8 +11,29 @@ def ping(host,count):
     except subprocess.CalledProcessError:
         return False
 
+def xlsx_process(file_name,sheet_index):
+    #Import xlrd library for read xlsx files
+    try:
+        import xlrd
+    except:
+        #If library not exist install library
+        subprocess.call(["pip3","install","xlrd"])
+    
+    #Open Workbook
+    try:
+        wb = xlrd.open_workbook(file_name)
+    except:
+        print("Wrong file name")
+        sys.exit(-1) #Exit with error code
+    
+    try:
+        sheet = wb.sheet_by_index(sheet_index)
+    except:
+        print("Number of index [{}] not found".format(sheet_index))
+        sys.exit(-1) #Exit with error code
+
 def main():
-    ping("10.0.2.1",2)
+    xlsx_process("host_list.xlsx",0)
 
 if __name__ == "__main__":
     main()
