f0af7f3958701270fd50312928ade4c8c9d87e90
keraattin <kerem-oruc@outlook.com>
2020-01-30 22:25:57 +0300

    Argument parser added

--- pinger.py
+++ pinger.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import sys
+from argparse import ArgumentParser
 
 # Ping command
 def ping(host,count):
@@ -33,7 +34,25 @@ def xlsx_process(file_name,sheet_index):
         sys.exit(-1) #Exit with error code
 
 def main():
-    xlsx_process("host_list.xlsx",0)
+    #Arguments
+    parser = ArgumentParser(description="Ping hosts from files")
+    parser.add_argument("-f","--filename", type=str, help="Column of ip addresses", required=True)
+    parser.add_argument("-s","--sheet", default=0, help="Sheet index[default = 0]")
+    args = parser.parse_args()
+
+    #File Name
+    if args.filename:
+        file_name = str(args.filename)
+    else:
+        print("File name and sheet must be entered.")
+
+    #Sheet Index
+    if args.sheet:
+        sheet_index = int(args.sheet)
+    else:
+        sheet_index = 0
+
+    xlsx_process(file_name,sheet_index)
 
 if __name__ == "__main__":
     main()
