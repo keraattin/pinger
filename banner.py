00b59eb11853d4859e73d9f877abaeecd5003576
keraattin <kerem-oruc@outlook.com>
2020-02-04 21:22:48 +0300

    Banner updated

--- banner.py
+++ banner.py
@@ -3,12 +3,13 @@
 import colors
 
 def show_banner():
-    print(colors.LIGHT_GREEN+"        _                               "+colors.NC)
-    print(colors.LIGHT_GREEN+"       (_)                              "+colors.NC)
-    print(colors.LIGHT_GREEN+" _ __   _  _ __    __ _   ___  _ __     "+colors.NC)
-    print(colors.LIGHT_GREEN+"| '_ \ | || '_ \  / _` | / _ \| '__|    "+colors.NC)
-    print(colors.LIGHT_GREEN+"| |_) || || | | || (_| ||  __/| |       "+colors.NC)
-    print(colors.LIGHT_GREEN+"| .__/ |_||_| |_| \__, | \___||_|       "+colors.NC)
-    print(colors.LIGHT_GREEN+"| |                __/ |                "+colors.NC)
-    print(colors.LIGHT_GREEN+"|_|               |___/                 "+colors.NC)
+    print(colors.BOLD+colors.LIGHT_GREEN+"        _                               "+colors.NC)
+    print(colors.BOLD+colors.LIGHT_GREEN+"       (_)                              "+colors.NC)
+    print(colors.BOLD+colors.LIGHT_GREEN+" _ __   _  _ __    __ _   ___  _ __     "+colors.NC)
+    print(colors.BOLD+colors.LIGHT_GREEN+"| '_ \ | || '_ \  / _` | / _ \| '__|    "+colors.NC)
+    print(colors.BOLD+colors.LIGHT_GREEN+"| |_) || || | | || (_| ||  __/| |       "+colors.NC)
+    print(colors.BOLD+colors.LIGHT_GREEN+"| .__/ |_||_| |_| \__, | \___||_|       "+colors.NC)
+    print(colors.BOLD+colors.LIGHT_GREEN+"| |                __/ |                "+colors.NC)
+    print(colors.BOLD+colors.LIGHT_GREEN+"|_|               |___/                 "+colors.NC)
+    print(colors.BOLD+colors.LIGHT_GREEN+"\t\t\t\tv1.1"+colors.NC)
     print("")
